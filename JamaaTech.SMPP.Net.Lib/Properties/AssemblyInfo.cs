90f7b13bf64445221b88dc9ffaf17f69d4eb3749
Adham Awadhi <adhamawadhi@hotmail.com>
2018-03-10 04:39:20 +0300

    v1.7.2

--- JamaaTech.SMPP.Net.Lib/Properties/AssemblyInfo.cs
+++ JamaaTech.SMPP.Net.Lib/Properties/AssemblyInfo.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a6ed1854-e4d7-43c4-8695-e6200045620b")]
-[assembly: AssemblyVersion("1.7.1.0")]
-[assembly: AssemblyFileVersion("1.7.1.0")]
+[assembly: AssemblyVersion("1.7.2.0")]
+[assembly: AssemblyFileVersion("1.7.2.0")]
 
 // Version information for an assembly consists of the following four values:
 //
